2f318fcbb9a9373d10a8f660e6ed06b3b74cd89a
Yuin <yhuang1223@gmail.com>
2013-10-11 13:09:28 +0100

    Created Yummy_Food app

---
+++ README.md
@@ -0,0 +1,13 @@
+## Yummy Food
+
+#### Description:
+Yummy Food is an Rails app similar to Yelp, which list restaurant and reviews. 
+
+#### Features
+- it can create a new restaruant using a form - a restaurant has a name and description
+- There should be an index page listing all restaurants
+- You can click on a restaurant to go to that restaurant's page
+- You can write a review of a restaurant. A review is composed of some text (e.g. "The food here is great"), and a score from 1-5
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YummyFood::Application.config.secret_key_base = 'fb06b045c4f36e6ef9f411526a89e67f4d86350c22f5439070938e5c32c18600b907269ba6aff4a5ffbb69c335e0de5b9a01aaaee43ac7b1d79a404f223dff04'
